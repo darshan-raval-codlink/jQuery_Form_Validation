079b39015fd0ada2712f726bf695f508d4649fcc
unknown <ravaldarshan237200@gmail.com>
2022-07-06 18:01:52 +0530

    file uplode

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+$.validator.addMethod("strongpassword",function(value){
+	return /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/.test(value);
+},"Please Enter Strong Password");
+
+$.validator.addMethod('filesize', function(value, element, param) {
+    return this.optional(element) || (element.files[0].size <= param)
+});
+
 jQuery('#frm').validate({
 	rules:{
 		name:"required",
@@ -7,16 +15,25 @@ jQuery('#frm').validate({
 		},
 		password:{
 			required:true,
-			minlength:5
+			minlength:8
 		},
 		cpassword:{
 			required:true,
-			minlength:8
+			minlength:8,
+			equalTo: "#password"
 		},
 		phonenumber:{
 			required:true,
 			minlength:10
 		}, 
+		// dob:{
+		// 	required:true,
+		// 	date:true
+		// }, 
+		gender: {
+			required:true,
+		}
+	
 		
 	},messages:{
 		name:"Please enter your name",
@@ -26,16 +43,18 @@ jQuery('#frm').validate({
 		},
 		password:{
 			required:"Please enter your password",
-			minlength:"Password must be 5 char long"
+			minlength:"Password must be 8 char long"
 		},
 		cpassword:{
 			required:"Please re-enter your password",
-			minlength:"Password must be 8 char long"
+			minlength:"Password must be 8 char long",
+			equalTo: "password must be same"
 		},
 		phonenumber:{
 			required:"Please enter your phone number",
 			minlength:"phone number must be 10 char long"
 		},
+		
 	},
 	submitHandler:function(form){
 		form.submit();
